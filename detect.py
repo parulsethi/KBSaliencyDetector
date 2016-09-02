fixed scaling issue

detect.py
@@ -8,14 +8,11 @@ import numpy as np
 def kbdetect(im,windows,mask):
     im = skimage.img_as_ubyte(im)
     nr,nc = im.shape
-
     # find pixels that we are going to examine
     r,c = np.nonzero(mask)
     nPix = len(r)
-
     # get how many scales we are doing
     nScales = len(windows)
-
     QUANTIZATION = 16
     edges = [_ for _ in range(0,257,QUANTIZATION)]
 
@@ -24,83 +21,79 @@ def kbdetect(im,windows,mask):
     entropy = np.zeros((nScales, nPix))
 
     out_scales = nScales
-
     # now iterate
-    for s_count in range(1,nScales):
+    for s_count in range(nScales):
         win_size = windows[s_count]
-        this_win = int((win_size)/2);
+        this_win = int((win_size)/2)
 
         # if scale is too big(more than half image size), then exclude that
-        if this_win+1 > nr/2 or this_win+1 > nc/2:
-            out_scales = s_count-1
-            print("breaaaaaaaaaaaaaaaaaaaak")
-            break
+        # if this_win+1 > nr/2 or this_win+1 > nc/2:
+        #     out_scales = s_count-1
+        #     print("breaaaaaaaaaaaaaaaaaaaak")
+        #     break
 
-        for i in range(1,nPix):
+        for i in range(nPix):
             # ignore points too close to the edge of the image
-            # if s_count == 2 and r(i) == 93 and c(i) == 93:
+            # if s_count == 1 and r[i] == 93 and c[i] == 93:
             #     dummy = 1
 
-            min_r = r[i]-this_win
-            min_c = c[i]-this_win
+            min_r = r[i] - this_win
+            min_c = c[i] - this_win
             max_r = min_r + win_size-1
             max_c = min_c + win_size-1
 
-            if min_r < 1:
-                min_r = 1
+            if min_r < 0:
+                min_r = 0
 
             if max_r > nr:
                 max_r = nr
 
-            if min_c < 1:
-                min_c = 1
+            if min_c < 0:
+                min_c = 0
 
             if max_c > nc:
                 max_c = nc
 
-            # compute the histogram of intensity values in this region
-            patch = im[min_r:max_r,min_c:max_c]
-            h = np.histogram(patch,bins=edges)           
-            h = h[0]
-            h[-1] = h[-1]-1
-            h = np.array([_/sum(h) for _ in h])
+            # print(min_r,min_c ,max_r ,max_c )
 
+            # compute the histogram of intensity values in this region
+            patch = im[min_r:max_r+1,min_c:max_c+1]
+            h = np.histogram(patch,bins=edges)
+
+            h = h[0]
+            # print(h)
+            h = np.array([_/sum(h) for _ in h])
+            # print(h)
             # index of histogram values greater than zero
             idx = np.nonzero(h > 0)
             entropy[s_count,i] = -sum(h[idx]*np.log(h[idx]))
 
-            if s_count >= 2:
+            if s_count >= 1:
                 dif = abs(h-last_h[:,i])
                 factor = windows[s_count]**2/(2*windows[s_count]-1)
                 # factor1 = (windows[s_count]+1)**2/(2*windows[s_count]+1)
                 ss_x[s_count,i] = factor * sum(dif);
                 # ss_x_new[s_count,i] = factor1*sum(dif);
 
-                if s_count == 2:
+                if s_count == 1:
                     ss_x[s_count-1,i] = ss_x[s_count,i]
                     #  New modification - it can also be zero as it is not going to matter
-                    # ss_x_new[s_count-1,i] = ss_x_new[s_count,i] 
+                    # ss_x_new[s_count-1,i] = ss_x_new[s_count,i]
 
             last_h[:,i] = h
-
 
     # now find local maxima in scale space by looking at the second derivative
     # being less than zero. calculate the weights and smooth since the first
     # derivative calculation will be noisy
 
-    fxx = np.array([1,-2,1])
+    fxx = (np.array([1,-2,1]))
 
-    weight = ss_x[1:out_scales,:]
-    # weight1 = ss_x_new(1:out_scales,:);
-    # # New modification - change the weight 
-    # temp=weight1(:,2:end)
-    # temp(:,end+1)=zeros(size(temp,1),1)
-    # new_weight = (weight+temp)/2
-    # new_weight(:,end)=new_weight(:,end-1)
-    x = entropy[1:out_scales,:].T
+    weight = ss_x
+
+    x = entropy.T
     ss_xx = ndimage.correlate1d(x,fxx,mode='nearest')
     ss_xx = ss_xx.T
-    ss_xx[1,:] = 0
+    ss_xx[0,:] = 0
     int_pts = np.nonzero(ss_xx < 0)
 
     # weight these points by the weighting function, which is the scale window
@@ -108,16 +101,17 @@ def kbdetect(im,windows,mask):
 
     # New modification - change 'weights' to 'new_weights'
     # regions.
-    
+
     gamma = entropy[int_pts]*weight[int_pts]
     # [scales,locs] = ind2sub([out_scales nPix], int_pts);
-    s = int_pts[0]
+    print((int_pts[0]).shape)
+    windows = np.array(windows)
+    s = windows[int_pts[0]]
     r = r[int_pts[1]]
     c = c[int_pts[1]]
 
     # regions.scale = np.transpose(windows(scales))
     # regions.r = r[locs]
     # regions.c = c[locs]
-
+    print(s)
     return gamma,s,r,c
-

extract.py
@@ -1,24 +1,32 @@
 from detect import kbdetect
 from prune import kbprune
 from show import kbshow
-from skimage import io,transform,color
+from skimage import io,transform,color,data
 import numpy as np
+from PIL import Image
 
-im = io.imread('background1.jpg')
+# def rgb2gray(rgb):
+#
+#     r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+#     gray = 0.2989*r + 0.5870*g + 0.1140*b
+#
+#     return gray
+
+im = io.imread('panda1.jpg')
 im = color.rgb2gray(im)
+# print(im[0:5,0:5])
 
 row,column = im.shape
-
 min_size = min(row,column)
 
-if min_size == row: 
+if min_size == row:
     temp1 = 100/row
     temp2 = int(column*temp1)
-    im = transform.resize(im,(100,temp2))
+    im = transform.resize(im,(100,temp2+1))
 else:
     temp1 = 100/column
     temp2 = int(row*temp1)
-    im = transform.resize(im,(temp2,100))
+    im = transform.resize(im,(temp2+1,100))
 
 nr,nc = im.shape
 
@@ -38,8 +46,8 @@ nr,nc = im.shape
 #     windows = [10:2:25];
 # end
 
-K = 5
-v_th = 1
+K = 7
+v_th = 9
 # scales
 windows = [_ for _ in range(10,25,2)]
 mask = np.ones((nr,nc))

prune.py
@@ -17,9 +17,8 @@ def kbprune(candidates,K,v_th):
     cc = candidates[3]
 
     # apply a global threshold to the gamma vals
-    thresh_val = .6 * max(cgamma)
+    thresh_val = .7 * max(cgamma)
 
-    # indices = find(cgamma > thresh_val)
     indices = np.nonzero(cgamma > thresh_val)
 
     tgamma = cgamma[indices]
@@ -51,36 +50,36 @@ def kbprune(candidates,K,v_th):
 
 
     # fill it with distances
-    for i in range(1,n):
+    for i in range(n):
         pt = pts[i][:]
 
         dists = np.sqrt(((pts-np.tile(pt,(pts.shape[0],1)))**2).sum(axis=1))
 
         D[i,:] = dists.T
-        D[:,i] = dists        
+        D[:,i] = dists
 
     nReg = 0
     regions = []
     pos = np.zeros((3,K+1))
 
     # now do the pruning process
-    for i in range(1,n):
+    for i in range(n):
         index = i
 
-        pos[0,1] = tc[index]
-        pos[1,1] = tr[index]
-        pos[2,1] = tscale[index]
+        pos[0,0] = tc[index]
+        pos[1,0] = tr[index]
+        pos[2,0] = tscale[index]
 
         sidx = np.argsort(D[index,:])
 
-        for j in range(1,K):
+        for j in range(K):
             pos[0,j+1] = tc[sidx[j+1]]
             pos[1,j+1] = tr[sidx[j+1]]
             pos[2,j+1] = tscale[sidx[j+1]]
 
         cent = np.array([np.mean(pos,axis=1)])
-        
-        v = np.var(np.sqrt( ((pos-np.tile(cent.T,(1,K+1)))**2).sum(axis=0) ))
+
+        v = np.var(np.sqrt(((pos-np.tile(cent.T,(1,K+1)))**2).sum(axis=0)))
 
         # if v > v_th:
         #     continue
@@ -106,4 +105,6 @@ def kbprune(candidates,K,v_th):
             rscale = np.append(rscale,cent[2])
             rgamma = np.append(rgamma,tgamma[index])
 
+
+    # print("tscale",tscale)
     return rgamma,rscale,rr,rc